711b01c34ac0c0be4c18dbb35b5b70d74e7b62a7
gkirber <george.kirber@gmail.com>
2026-04-23 23:22:44 +0200

    Filter Redux slice and filter by title

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,10 +1,12 @@
 import { BsBookmarkStar, BsBookmarkStarFill } from 'react-icons/bs'
 import { useDispatch, useSelector } from 'react-redux'
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
+import { selectTitleFilter } from '../../redux/slices/filterSlice'
 import './BookList.css'
 
 const BookList = () => {
 	const books = useSelector(state => state.books)
+	const titleFilter = useSelector(selectTitleFilter)
 	const dispatch = useDispatch()
 
 	const handleDeleteBook = id => {
@@ -15,6 +17,13 @@ const BookList = () => {
 		dispatch(toggleFavorite(id))
 	}
 
+	const filteredBooks = books.filter(book => {
+		const matchesTitle = book.title
+			.toLowerCase()
+			.includes(titleFilter.toLowerCase())
+		return matchesTitle
+	})
+
 	return (
 		<div className='app-block book-list'>
 			<h2>Book List</h2>
@@ -22,18 +31,18 @@ const BookList = () => {
 				<p>No books available</p>
 			) : (
 				<ul>
-					{books.map((book, i) => (
+					{filteredBooks.map((book, i) => (
 						<li key={book.id}>
 							<div className='book-info'>
 								{++i}. {book.title} by <strong>{book.author}</strong>
 							</div>
 							<div className='book-actions'>
 								<span onClick={() => handleToggleFavorite(book.id)}>
-								{book.isFavorite ? (
-									<BsBookmarkStarFill className='star-icon' />
-								) : (
-									<BsBookmarkStar className='star-icon' />
-								)}
+									{book.isFavorite ? (
+										<BsBookmarkStarFill className='star-icon' />
+									) : (
+										<BsBookmarkStar className='star-icon' />
+									)}
 								</span>
 								<button onClick={() => handleDeleteBook(book.id)}>
 									Delete
